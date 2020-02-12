35204bae9c415c7cc5851b32da248b99a5313764
abcde090 <229243386@qq.com>
2020-02-13 00:58:29 +1000

    Add search bar, nav bar

---
+++ client/pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+<main class="listingPage">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-xl-2 col-lg-3 md-4 col-sm-4">
+        <!-- Sidebar -->
+      </div>
+
+      <!-- Main Content -->
+      <div class="col-xl-10 col-lg-9 md-8 col-sm-8">
+        <FeatureProduct />
+      </div>
+
+    </div>
+  </div>
+</main>
+</template>
+
+<script>
+import FeatureProduct from '~/components/FeatureProduct'
+export default {
+  components: {
+    FeatureProduct
+  }
+};
+</script>
